42bd6478ce58bb038da1aa7dbfee09739513ec47
KueiDi Jang <quidditchang@gmail.com>
2026-05-14 22:03:56 +0800

    fix: exclude nested repos from index

--- tracer_non_newtonian/python/pythia-0.8.1.7-py2.5.egg/opal/conf/urls/admin_password_reset.py
+++
@@ -1,6 +0,0 @@
-from opal.conf.urls.defaults import *
-
-urlpatterns = patterns('opal.views',
-    (r'^$', 'registration.passwords.password_reset', {'is_admin_site' : True}),
-    (r'^done/$', 'registration.passwords.password_reset_done'),
-)
