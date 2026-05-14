40ccdcd43413d0541a387ff26f8e1ce45de85d5b
KueiDi Jang <quidditchang@gmail.com>
2026-05-14 22:02:13 +0800

    fix: exclude nested repos from index

--- tracer_non_newtonian/python/pythia-0.8.1.7-py2.5.egg/opal/conf/urls/i18n.py
+++
@@ -1,5 +0,0 @@
-from opal.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'^setlang/$', 'opal.views.i18n.set_language'),
-)
--- tracer_non_newtonian/python/pythia-0.8.1.7-py2.5.egg/opal/conf/urls/shortcut.py
+++
@@ -1,5 +0,0 @@
-from opal.conf.urls.defaults import *
-
-urlpatterns = patterns('opal.views',
-    (r'^(?P<content_type_id>\d+)/(?P<object_id>\d+)/$', 'defaults.shortcut'),
-)
