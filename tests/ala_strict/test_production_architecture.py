3330bea883b519fbc06bd1f5e50304a9e6ef1ecf
KueiDi Jang <quidditchang@gmail.com>
2026-08-04 19:00:21 +0800

    test: extend strict ALA global convergence probe

--- tests/ala_strict/test_production_architecture.py
+++ tests/ala_strict/test_production_architecture.py
@@ -110,7 +110,7 @@ class StrictProductionArchitectureTest(unittest.TestCase):
         )
         self.assertRegex(
             strict_text,
-            r"(?m)^\s*ala_coupled_debug_stop_iteration\s*=\s*5\s*$",
+            r"(?m)^\s*ala_coupled_debug_stop_iteration\s*=\s*10\s*$",
         )
         self.assertRegex(
             strict_text,
