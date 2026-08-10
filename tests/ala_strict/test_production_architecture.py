0f635bb8d4db4c61453ef9896d6064b4c39f25d1
KueiDi Jang <quidditchang@gmail.com>
2026-08-10 11:01:26 +0800

    test: track current-rheology gamma ten calibration

--- tests/ala_strict/test_production_architecture.py
+++ tests/ala_strict/test_production_architecture.py
@@ -300,7 +300,7 @@ class StrictProductionArchitectureTest(unittest.TestCase):
         )
         self.assertRegex(
             strict_text,
-            r"(?m)^\s*ala_augmented_lagrangian_gamma\s*=\s*1\.0\s*$",
+            r"(?m)^\s*ala_augmented_lagrangian_gamma\s*=\s*10\.0\s*$",
         )
         self.assertRegex(
             strict_text,
