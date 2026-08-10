e9ce3495080c124bea8c194537aa95babf18a1a0
KueiDi Jang <quidditchang@gmail.com>
2026-08-10 12:50:43 +0800

    test: extend hybrid strict ALA acceptance budget

--- tests/ala_strict/test_production_architecture.py
+++ tests/ala_strict/test_production_architecture.py
@@ -145,7 +145,7 @@ class StrictProductionArchitectureTest(unittest.TestCase):
         )
         self.assertRegex(
             strict_text,
-            r"(?m)^\s*ala_coupled_debug_stop_iteration\s*=\s*20\s*$",
+            r"(?m)^\s*ala_coupled_debug_stop_iteration\s*=\s*200\s*$",
         )
         self.assertRegex(
             strict_text,
